cdfa281cf4eaf702822f1c58df25de522f589715
Akash Trivedi <akash@citizencode.io>
2017-02-01 11:56:04 -0800

    Successfully retrieving access token; need to fix session

--- server/routes.js
+++ server/routes.js
@@ -3,11 +3,7 @@
 const controller = require('./login_controller.js')
 
 function router(app) {
-  app.get("/", controller.redirect)
-
   app.get("/oauth", controller.verifyUser)
-  
-  app.post("/me", controller.userInfo)
 }
 
 module.exports = router
