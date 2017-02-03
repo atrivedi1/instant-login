8952fb6cd0ec468103609b8b379df9ddd36bea3f
Akash Trivedi <akash@citizencode.io>
2017-02-02 22:40:16 -0800

    Added app urls to env variables and renamed process vars in controller

--- server/login_controller.js
+++ server/login_controller.js
@@ -8,8 +8,8 @@ const CLIENT_ID = process.env.CLIENT_ID || cleverAPI.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET || cleverAPI.CLIENT_SECRET;
 const DISTRICT_ID = process.env.DISTRICT_ID || cleverAPI.DISTRICT_ID;
 
-const APP_LOGIN_URL = process.env.APP_URL || 'http://localhost:3000';
-const APP_OAUTH_URL = process.env.OAUTH_URL || 'http://localhost:3000/oauth';
+const APP_LOGIN_URL = process.env.APP_LOGIN_URL || 'http://localhost:3000';
+const APP_OAUTH_URL = process.env.APP_OAUTH_URL || 'http://localhost:3000/oauth';
 
 const API_PREFIX = 'https://api.clever.com';
 const API_OAUTH_TOKEN_URL = 'https://clever.com/oauth/tokens';
